e9dc614dd6ff6d63069354eb9e02cb5c5ae1428a
DT <dariotaoasus@gmail.com>
2023-06-28 15:48:08 -0400

    Updated app.js

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
-console.log("hello world")
+// console.log("hello world")
+
+var MongoClient = require('mongodb').MongoClient;
+
+var url = "mongodb+srv://dariotao01:U2lz6UlVYc4FVVSh@cluster0.ztilryq.mongodb.net/?retryWrites=true&w=majority";
+
+MongoClient.connect(url, function(err, db) {
+    if (err) throw err;
+    var dbo = db.db("UofTCourses");
+    dbo.collection("courses").findOne({}, function(err, result) {
+      if (err) throw err;
+      console.log(result.name);
+      db.close();
+    });
+  });
