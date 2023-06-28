4860369f94d11889c4c1540eb20f9841ccf3b79e
lidickson1 <52766315+lidickson1@users.noreply.github.com>
2023-06-28 16:11:05 -0400

    Fix

--- app.js
+++ app.js
@@ -1,15 +1,28 @@
-// console.log("hello world")
-
-var MongoClient = require('mongodb').MongoClient;
-
-var url = "mongodb+srv://dariotao01:U2lz6UlVYc4FVVSh@cluster0.ztilryq.mongodb.net/?retryWrites=true&w=majority";
-
-MongoClient.connect(url, function(err, db) {
-    if (err) throw err;
-    var dbo = db.db("UofTCourses");
-    dbo.collection("courses").findOne({}, function(err, result) {
-      if (err) throw err;
-      console.log(result.name);
-      db.close();
-    });
-  });
+const { MongoClient, ServerApiVersion } = require("mongodb");
+// Replace the placeholder with your Atlas connection string
+const uri =
+    "mongodb+srv://dariotao01:U2lz6UlVYc4FVVSh@cluster0.ztilryq.mongodb.net/";
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    },
+});
+async function run() {
+    try {
+        // Connect the client to the server (optional starting in v4.7)
+        await client.connect();
+        // Send a ping to confirm a successful connection
+        const res = await client
+            .db("UofTCourses")
+            .collection("courses")
+            .findOne({});
+        console.log(res);
+    } finally {
+        // Ensures that the client will close when you finish/error
+        await client.close();
+    }
+}
+run().catch(console.dir);
